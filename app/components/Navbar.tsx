59d93ba55f5841830fde582004a6e41e4487a19d
Don-Debug <elision.q23@gmail.com>
2023-09-30 21:09:16 +0200

    ok

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div>This is going to be the navbar and the page heading as well</div>
+    </div>
+  );
+}
---
+++ app/tiktok/page.tsx
@@ -0,0 +1,7 @@
+export default function TikTok() {
+  return (
+    <div>
+      <p>Tik Tok Wrapper</p>
+    </div>
+  );
+}

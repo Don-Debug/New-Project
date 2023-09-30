c778cf9e532ede55cbe383a5d048fc469ed41285
ergin.dapaj <ergin.dapaj@bgasoft.com>
2023-10-01 00:17:28 +0200

    first navbar test push

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Home";
-
+import { ChakraProvider } from '@chakra-ui/react'
 export default function Home() {
   return (
     <div>
+      <ChakraProvider>
       <Hero />
+      </ChakraProvider>
     </div>
   );
 }

03bb25ee33647593ec5bf56c0b3dafa60c531b5b
Don-Debug <elision.q23@gmail.com>
2023-10-01 19:27:37 +0200

    ok

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,57 +1,58 @@
-"use client"
-import { Box, Flex, Text, Button, Avatar, IconButton, Tooltip } from "@chakra-ui/react";
-import { useRouter } from 'next/navigation';
-import { ViewIcon, CloseIcon } from '@chakra-ui/icons';
-import { useState } from 'react';
+"use client";
+
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Navbar() {
-  const router = useRouter();
-  const [showBalance, setShowBalance] = useState(false);
-  const balance = "$43"; // Replace with your actual balance
-  
   return (
     <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="1.5rem"
-      bg="teal.500"
-      color="black"
+      justifyContent="space-around"
+      p="2rem"
+      background="#2f2f2f"
+      color="white"
     >
-      <Flex align="center" mr={5}>
-        <Text fontSize="lg" fontWeight="bold">
-          AMONGUS
-        </Text>
-      </Flex>
-
       <Box
+        py="1rem"
+        px="3rem"
+        cursor="pointer"
+        borderBottom="2px"
+        borderColor="white"
         display="flex"
-        width="auto"
-        alignItems="center"
-        flexGrow={1}
-        justifyContent="flex-end"
+        justifyContent="center"
       >
-        <Tooltip label={showBalance ? "Hide balance" : "Show balance"} placement="top">
-          <IconButton 
-            aria-label={showBalance ? "Hide balance" : "Show balance"} 
-            icon={showBalance ?  <Text>{balance}</Text> : <ViewIcon />} 
-            colorScheme="black" 
-            variant="outline" 
-            mr={4} 
-            onClick={() => setShowBalance(!showBalance)}
-          />
-        </Tooltip>
-        
-        {showBalance}
-        
-        <Avatar 
-          size="md" 
-          name="Profile Picture" 
-          src="" 
-          onClick={() => router.push('/profile')}
-          cursor="pointer"
-        />
+        Instagram
+      </Box>
+      <Box
+        py="1rem"
+        px="3rem"
+        cursor="pointer"
+        borderBottom="2px"
+        borderColor="white"
+        display="flex"
+        justifyContent="center"
+      >
+        Tik Tok
+      </Box>
+      <Box
+        py="1rem"
+        px="3rem"
+        cursor="pointer"
+        borderBottom="2px"
+        borderColor="white"
+        display="flex"
+        justifyContent="center"
+      >
+        You Tube
+      </Box>
+      <Box
+        py="1rem"
+        px="3rem"
+        cursor="pointer"
+        borderBottom="2px"
+        borderColor="white"
+        display="flex"
+        justifyContent="center"
+      >
+        X
       </Box>
     </Flex>
   );
